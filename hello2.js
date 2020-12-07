2f94c7ed372d52baee3113b3f53af613aca96daf
Nick Murphy <71105164+nicksmurf7@users.noreply.github.com>
2020-12-07 16:35:54 -0500

    Add files via upload

---
+++ hello2.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+
+console.log("This goes to the console window");
+http.createserver(function(req, res) {
+    res.writeHead(200,{'Content-Type': 'text/html'});
+    res.write("<h2> Hello World </h2>");
+    res.write("Success! This app is deployed online");
+    res.end();
+}).listen(port);
